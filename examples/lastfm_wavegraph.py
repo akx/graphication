02d59da1e1b8dcdc02960095072f7111186a0abd
Aarni Koskela <akx@iki.fi>
2018-07-03 14:44:31 +0300

    Remove lastfm API, as the web service probably hasn't worked in a while

--- examples/lastfm_wavegraph.py
+++ examples/lastfm_wavegraph.py
@@ -2,21 +2,30 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+import os
 import sys
 import time
 import random
-import graphication.lastfm as lastfm
-from graphication import *
+import datetime
+from graphication import SeriesSet, Series, css, Colourer, FileOutput, AutoWeekDateScale, Label
 from graphication.wavegraph import WaveGraph
-
-# Grab the last year's worth for the given username
-username = sys.argv[1]
 
 
 def p(x): print("%3.0f%% complete" % (x*100,))
 
 
-artists = lastfm.artist_range_chart(username, time.time() - 31557600, time.time(), callback=p, dated=True)
+def get_fake_lastfm_data():
+    ts1 = datetime.datetime(2018, 4, 1).timestamp()
+    ts2 = datetime.datetime(2018, 4, 7).timestamp()
+    ts3 = datetime.datetime(2018, 4, 14).timestamp()
+
+    return {
+        'Artist 1': [(ts1, 100), (ts2, 200), (ts3, 10)],
+        'Artist 2': [(ts1, 200), (ts2, 100), (ts3, 80)],
+        'Artist 3': [(ts2, 200), (ts3, 100)],
+    }
+
+artists = get_fake_lastfm_data().items()
 
 # Create the series set and throw in the artists
 
@@ -42,11 +51,11 @@ scale = AutoWeekDateScale(series_set)
 
 # OK, render that.
 wg = WaveGraph(series_set, scale, style, label_curves=True)
-lb = Label(username, style)
+lb = Label("username", style)
 
 width = 30*len(list(series_set.keys()))
 output.add_item(lb, x=10, y=5, width=width-20, height=20)
 output.add_item(wg, x=0, y=30, width=width, height=200)
 
 # Save the images
-output.write("pdf", "%s.pdf" % username)
+output.write("pdf", "fake_lastfm.pdf")
